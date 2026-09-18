e3df5e361a03710a72524d6c90504d2f9814fb02
Parmeshwar-12368 <parmeshwarray38@gmail.com>
2026-09-18 20:55:46 +0530

    Time: 23 ms (64.94%) | Memory: 88.4 MB (43.15%) - LeetSync

---
+++ 525-contiguous-array/contiguous-array.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int findMaxLength(vector<int>& nums) {
+        int n = nums.size();
+        unordered_map<int,int> m;
+        m[0]=-1;
+
+        int sum = 0;
+        int ans = 0;
+
+        for(int i=0; i<n; i++){
+            sum += 2*nums[i]-1;
+
+            if(m.find(sum) != m.end()){
+                ans = max(ans, i-m[sum]);
+            } else {
+                m[sum]=i;
+            }
+        }
+        return ans;
+    }
+};
